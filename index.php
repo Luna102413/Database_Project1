d4148a62976868779903c1b50b2b2a830276a6c8
Luna102413 <57510524+Luna102413@users.noreply.github.com>
2020-02-13 14:29:03 -0600

    Base template

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Manage Data in University Database</title>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    </head>
+    <body>
+        <main>
+            <table>
+                <thead>
+
+                </thead>
+                <tbody>
+
+                </tbody>
+                <tfooter>
+                    
+                </tfooter>
+            </table>
+        </main>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
+    </body>
+</html>
